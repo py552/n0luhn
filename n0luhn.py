3d631437e2e4d6f1ea9e24d7cd1f23217939b176
f552p69 <f552p69@gmail.com>
2020-12-22 22:43:44 +0400

    Python module could be used as independent utility

--- n0luhn.py
+++ n0luhn.py
@@ -1,7 +1,15 @@
-# Implementation of Luhn algorithm for checking and generation tail control digit of card number
-# https://en.wikipedia.org/wiki/Luhn_algorithm
+import sys
+import os
 
-def calcLuhn(cardNo: str, isCurrentCharEven: bool = False):   # isCurrentCharEven = False for checking, True for calculation Luhn number
+def calcLuhn(cardNo: str, isCurrentCharEven: bool = False):
+    """
+    cardNo:
+           cardNo for checking if isCurrentCharEven = False
+           cardNoWithoutLuhn for calculation Luhn number if isCurrentCharEven = True
+    isCurrentCharEven:
+           False for checking
+           True for calculation Luhn number
+    """
     luhnNumber = 0
     for currentChar in cardNo[::-1]:    # Pass from right to left, so reverse cardNo
         currenDigit = ord(currentChar) - ord('0')
@@ -14,22 +22,19 @@ def calcLuhn(cardNo: str, isCurrentCharEven: bool = False):   # isCurrentCharEve
     luhnNumber = luhnNumber % 10
     if luhnNumber:  
         luhnNumber = 10 - luhnNumber    # Calculate Luhn number for generateLuhn(cardNoWithoutLuhn)
-    # If initial isCurrentCharEven == False 
+    # If initial isCurrentCharEven == False (called for checking)
     #     and luhnNumber == 0 then cardNo[-1] is correct Luhn number
     #     else cardNo[-1] is NOT correct Luhn number
     # else luhnNumber is calculated Luhn number
     return luhnNumber   
-
 def checkLuhn(cardNo: str):
     return False if calcLuhn(cardNo) else True
-
 def generateLuhn(cardNoWithoutLuhn: str):
     return cardNoWithoutLuhn + chr(calcLuhn(cardNoWithoutLuhn, True) + ord('0'))
-    
-if __name__=="__main__":
-    cardNo = "79927398710"
-    if (checkLuhn(cardNo)):
-        print("'%s' is a valid card number" % cardNo)
+
+if __name__ == "__main__":
+    # print(generateLuhn("444555oooooo777".replace("o","0")))
+    if len(sys.argv) == 2:
+        print(generateLuhn(sys.argv[1].replace("o","0")))
     else:
-        print("'%s' is NOT a valid card number" % cardNo)
-    print("'%s' is correct card number" % generateLuhn(cardNo[:-1]))
+        print("Usage: %s <First 15 digits of card no>" % os.path.split(sys.argv[0])[1])
